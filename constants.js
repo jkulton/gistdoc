e43c2e9faab66bdaa1b04a55dd4b558ef19b2f4f
Jon Kulton <jonkulton@gmail.com>
2022-01-09 19:23:16 -0500

    add gists to constants

--- constants.js
+++ constants.js
@@ -1,6 +1,9 @@
 const constants = {
   GITHUB_API_BASE_URL: 'https://api.github.com',
-  HOMEPAGE_GIST_ID: '59ff2b40f69fc7f4ea2fdd6b1a044648'
+  GIST: {
+    HOME: '59ff2b40f69fc7f4ea2fdd6b1a044648',
+    CHANGELOG: '2ff7302367044178af3eeaee4384a4ba'
+  }
 }
 
 export default constants;

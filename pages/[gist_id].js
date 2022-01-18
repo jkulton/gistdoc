cae76730a459967567315e3bfd13e844c0299443
Jon Kulton <jonkulton@gmail.com>
2022-01-18 00:04:24 -0500

    adjust theme-color hex

--- pages/[gist_id].js
+++ pages/[gist_id].js
@@ -45,7 +45,7 @@ function Page() {
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#222222" />
+        <meta name="theme-color" content="#1d1d1d" />
         <script defer data-domain="gistdoc.com" src="https://plausible.io/js/plausible.js"></script>
         {
           gistData?.public === false && <meta name="robots" content="noindex"/>

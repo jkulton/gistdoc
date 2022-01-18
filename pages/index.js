c4ca368cbfb9db86641a9ca6022ac8dd834c43f8
Jon Kulton <jonkulton@gmail.com>
2022-01-17 23:05:25 -0500

    add meta tags for og info

--- pages/index.js
+++ pages/index.js
@@ -16,11 +16,25 @@ export default function Home() {
   return (
     <div className={'home'}>
       <Head>
-        <meta name="description" content={'View gists in a clean blog-like format.'} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Gistdoc</title>
         <link rel="preconnect" href="https://user-images.githubusercontent.com/" />
         <script defer data-domain="gistdoc.com" src="https://plausible.io/js/plausible.js"></script>
+
+        <meta name="title" content="Gistdoc" />
+        <meta name="description" content="Quickly view GitHub markdown gists as simple blog-style pages" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://gistdoc.com/" />
+        <meta property="og:title" content="Gistdoc" />
+        <meta property="og:description" content="Quickly view GitHub markdown gists as simple blog-style pages" />
+        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://gistdoc.com/" />
+        <meta property="twitter:title" content="Gistdoc" />
+        <meta property="twitter:description" content="Quickly view GitHub markdown gists as simple blog-style pages" />
+        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
       </Head>
       <div className={'page-header'}>
         <Link href='/'>
